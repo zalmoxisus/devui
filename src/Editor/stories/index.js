e88d3e64a146c94e8abe3574f5e528a1c6d79d41
Zalmoxisus <zalmoxisus@gmail.com>
2016-11-23 18:49:32 +0200

    List Editor themes

--- src/Editor/stories/index.js
+++ src/Editor/stories/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { withKnobs, text, select, boolean } from '@kadira/storybook-addon-knobs';
 import Editor from '../';
+import themes from '../themes';
 import WithTabs from './WithTabs';
 
-const themes = {
-  default: 'default',
-  night: 'night',
-  'solarized dark': 'solarized dark',
-  'solarized light': 'solarized light',
-  'tomorrow-night-bright': 'tomorrow-night-bright'
-};
-
 const value = `
-var themes = ${JSON.stringify(themes, null, 2)};
+var themes = [];
 
 function getThemes() {
   return themes;
