c29bf5f6e530a639fded1e3454fd83a842e8b21c
Zalmoxisus <zalmoxisus@gmail.com>
2016-11-24 18:59:16 +0200

    Prevent importing styles in test environment

--- src/presets.js
+++ src/presets.js
@@ -1,9 +1,13 @@
+/* eslint-disable global-require */
 function requireAll(r) { r.keys().forEach(r); }
 
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/addon/fold/foldgutter';
 import 'codemirror/addon/fold/foldcode';
 import 'codemirror/addon/fold/brace-fold';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/addon/fold/foldgutter.css';
-requireAll(require.context('codemirror/theme/', false, /\.css/));
+
+if (process.env.NODE_ENV !== 'test') {
+  require('codemirror/lib/codemirror.css');
+  require('codemirror/addon/fold/foldgutter.css');
+  requireAll(require.context('codemirror/theme/', false, /\.css/));
+}
