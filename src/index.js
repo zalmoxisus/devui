1eea18467d8f2d50bf538aa9f8878fb5db28637c
Zalmoxisus <zalmoxisus@gmail.com>
2017-01-16 11:35:51 +0200

    Container component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+export { default as Container } from './Container';
 export { default as Button } from './Button';
 export { default as Dialog } from './Dialog';
 export { default as Editor } from './Editor';
