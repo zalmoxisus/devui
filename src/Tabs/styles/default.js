a87e9d7f97ef3c92ad9e6f24257c920df40786e6
Zalmoxisus <zalmoxisus@gmail.com>
2017-01-27 14:02:00 +0200

    Bordered tabs

--- src/Tabs/styles/default.js
+++ src/Tabs/styles/default.js
@@ -4,14 +4,16 @@ export const style = ({ theme, main }) => css`
   display: flex;
   flex: 0 0 30px;
   padding-left: 1px;
-  border-top: 1px solid ${theme.base01};
   background-color: ${theme.base01};
+  ${!main && `
+  border-top: 1px solid ${theme.base01};
+  border-bottom: 1px solid ${theme.base02};
+  `}
 
   > div:first-child {
     display: flex;
     align-items: flex-end;
     flex-wrap: nowrap;
-    overflow-x: auto;
 
     button {
       background-color: ${theme.base01};
@@ -20,7 +22,7 @@ export const style = ({ theme, main }) => css`
       min-height: 30px;
       padding: 2px 10px;
       margin-right: 1px;
-      border: 2px solid transparent;
+      border: ${main ? '2' : '1'}px solid transparent;
       cursor: pointer;
       text-align: center;
       overflow: hidden;
@@ -37,7 +39,12 @@ export const style = ({ theme, main }) => css`
     > [data-selected] {
       ${main ?
       `border-bottom: 2px solid ${theme.base0B};` :
-      `background-color: ${theme.base00};`
+      `
+      background-color: ${theme.base00};
+      border: 1px solid ${theme.base02};
+      border-bottom: 1px solid ${theme.base00};
+      box-shadow: 0 1px ${theme.base00};
+      `
       }
       color: ${theme.base07};
     }
