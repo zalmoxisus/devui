d74bb827d065f16485eaacdf1cfc75099d81f265
nndio <anastasia.diordiev@gmail.com>
2017-03-08 11:31:27 +0200

    Render tabs in SegmentedControl

--- src/SegmentedControl/SegmentedControl.js
+++ src/SegmentedControl/SegmentedControl.js
@@ -2,58 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import getStyles from '../utils/getStyles';
 import { style } from './styles';
 
-const ButtonWrapper = getStyles(style, 'div');
+const SegmentedWrapper = getStyles(style, 'div');
 
-export default class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.updateItems(props);
-  }
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.tabs !== this.props.tabs || nextProps.selected !== this.props.selected) {
-      this.updateItems(nextProps);
-    }
-  }
+export default class SegmentedControl extends Component {
   shouldComponentUpdate(nextProps) {
-    return nextProps.children !== this.props.children ||
-      nextProps.disabled !== this.props.disabled ||
-      nextProps.title !== this.props.title;
-  }
-
-  updateItems(props) {
-    const tabs = props.tabs;
-
-    this.tabsHeader = tabs.map(tab => {
-      const value = typeof tab.value !== 'undefined' ? tab.value : tab.name;
-      return (
-        <ButtonWrapper
-          key={value}
-          value={value}
-          onClick={this.onClick}
-        >
-          {tab.name}
-        </ButtonWrapper>
-      );
-    });
+    return nextProps.disabled !== this.props.disabled
   }
 
   render() {
     return (
-      <div>
-        {this.tabsHeader}
-      </div>
+      <SegmentedWrapper>
+        {this.props.tabs.map(tab =>
+          <button key={tab.name}>
+            {tab.name}
+          </button>
+        )}
+      </SegmentedWrapper>
     );
   }
 }
 
-Button.propTypes = {
+SegmentedControl.propTypes = {
   tabs: PropTypes.array.isRequired,
-  title: PropTypes.string,
   onClick: PropTypes.func,
-  type: PropTypes.string,
   disabled: PropTypes.bool
 };
 
-Button.defaultProps = {
-  tooltipPosition: 'top'
-};
--- src/SegmentedControl/styles/index.js
+++ src/SegmentedControl/styles/index.js
@@ -1,6 +1,7 @@
 import { css } from 'styled-components';
 
 export const style = ({ theme, type, disabled }) => css`
+  width: 100%;
   font-weight: 600;
   font-size: 1.1em;
   display: inline-block;
@@ -13,7 +14,7 @@ export const style = ({ theme, type, disabled }) => css`
   background-color: ${theme.base02};
   opacity: 0.7;
   ` : `
-  cursor: pointer;
+  cursor: handle;
   color: ${theme.base05};
   background-color: ${theme.base01};
   `}
