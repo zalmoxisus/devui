2bfa1b7a6b074ba6a840693639d57c65628509ee
Jhen <developer@jhen.me>
2017-10-26 19:17:52 +0800

    Fix `Accessing nonexistent addons channel` error of storybook

--- .storybook/themeAddon/Panel.js
+++ .storybook/themeAddon/Panel.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Form from '@storybook/addon-knobs/dist/components/PropForm';
 import styled from 'styled-components';
-import { EVENT_ID_DATA } from './';
-import { defaultState } from './theme';
+import { EVENT_ID_DATA, DEFAULT_THEME_STATE } from './constant';
 import { listSchemes, listThemes } from '../../src/utils/theme';
 
 const FormWrapper = styled.div`
@@ -18,13 +17,13 @@ const schemes = listSchemes();
 const themes = listThemes();
 
 export default class Panel extends React.Component {
-  state = defaultState;
+  state = DEFAULT_THEME_STATE;
 
   onChange = o => {
     const state = { [o.name.split(' ').slice(-1)[0]]: o.value };
     this.props.channel.emit(EVENT_ID_DATA, state);
     this.setState(state);
-  }
+  };
 
   render() {
     const { theme, scheme, light } = this.state;
